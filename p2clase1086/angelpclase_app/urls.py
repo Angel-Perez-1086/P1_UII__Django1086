1560f9c19b5c5aa9c4accd80a17709f84a97e1d8
Angel Perez <164534035+Angel-Perez-1086@users.noreply.github.com>
2024-10-01 22:42:08 -0600

    Delete p2clase1086 directory

--- p2clase1086/angelpclase_app/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.hola, name='angelpclase_app'),
-]
--- p2clase1086/angelpclase_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def hola(request):
-    return HttpResponse(" Hola angel respondiendo")
-# Create your views here.
